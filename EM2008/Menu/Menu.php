9923091da5a4ea57c00b6e90fe8cc7e0aaca9205
silvan.staeheli@gmx.ch <silvan.staeheli@gmx.ch@81f5e6eb-a649-0410-baa0-7d6264f9b901>
2009-07-28 15:35:04 +0000

    users online

--- EM2008/Menu/Menu.php
+++ EM2008/Menu/Menu.php
@@ -4,12 +4,14 @@ require_once('Datenbank/db.php');
 class Menu {
 
 	private $link = '';
-
+	
 	public function __construct() {
 		$this->link = Db::getConnection();
 	}
 
 	public function getMenu() {
+		$this->userOnline();
+		
 		$registered = $_SESSION['eingeloggt'];
 		if($registered) {
 			return 'Menu/layout/registered.tpl';
@@ -17,6 +19,61 @@ class Menu {
 		else {
 			return 'Menu/layout/notRegistered.tpl';
 		}
+	}
+	
+	private function userOnline(){
+		$this->link = Db::getConnection();
+		
+		$zeitspanne = 300; //Sekunden
+		$ip = $_SERVER['REMOTE_ADDR'];
+		$userid = isset($_SESSION['userid']) ? $_SESSION['userid'] : '0';
+		
+		//veraltete Einträge löschen
+		mysql_query("DELETE FROM useronline WHERE zeit < ".time()."");
+		
+		//Zeitpunkt erneuern
+		mysql_query("UPDATE useronline SET zeit = '".(time()+$zeitspanne)."', userfsid = $userid WHERE ip='".$ip."'");
+		
+		// ist der Besucher noch nicht eigetragen, so wird ein neuer Eintrag erzeugt.
+		$query = "SELECT ip from useronline WHERE ip='".$ip."'";
+		$daten = mysql_query($query);
+		
+		if(mysql_num_rows($daten) == 0) {
+			mysql_query("INSERT INTO useronline (ip,zeit,userfsid) VALUES ('$ip ','".(time()+$zeitspanne)."','$userid')");
+		}
+		
+		// die Zahl der Online-User ermitteln.
+		$result = mysql_query("SELECT count(*) FROM useronline");
+		
+		// namen der eingeloggten users ermitteln
+		$query_online_names = "SELECT userfsid from useronline";
+		$daten_online_names = mysql_query($query_online_names);
+		
+		$names = '';
+		$first = true;
+		
+		while($row = mysql_fetch_assoc($daten_online_names))
+		{
+			$userid = $row['userfsid'];
+			
+			if($userid != 0){
+				$actual_name = "SELECT nachname, vorname from user WHERE userid = $userid";
+				$actual_name_result = mysql_query($actual_name);
+				
+				while($row_user_name = mysql_fetch_assoc($actual_name_result))
+				{
+					if($first){
+						$names = "<b>davon eingeloggt:</b><br>";
+						$first = false;
+					}
+					$names = $names . $row_user_name['vorname'] . " " . $row_user_name['nachname'] . "<br>";
+				}
+			}	
+		}
+		if($names != ''){
+			$_SESSION['users_names'] = $names;
+		}
+		$_SESSION['users'] = "&nbsp;" .mysql_result($result,0)." User online";
 	}
 }
 ?>
--- EM2008/Menu/layout/notRegistered.tpl
+++ EM2008/Menu/layout/notRegistered.tpl
@@ -4,4 +4,20 @@
 	<li class="Startseite" id="mn_Standard"><a href="index.php?go=login"><span>Login</span></a></li>
 	<li class="Startseite" id="mn_Standard"><a href="index.php?go=register"><span>Registrieren</span></a></li>
 	<li class="Startseite" id="mn_Standard"><a href="index.php?go=rules"><span>Spielregeln</span></a></li>
-</ul>	
+	<?php
+		if(isset($_SESSION['users_names'])){
+	?>
+			<li class="Startseite" id="mn_Standard"><a class="tooltip" href="#"><?php echo $_SESSION['users'] ?><span><?php echo $_SESSION['users_names'] ?></span></a></li>
+	<?php
+		}
+		else{
+	?>
+			<li class="Startseite" id="mn_Standard"><a class="tooltip" href="#"><?php echo $_SESSION['users'] ?></a></li>
+	<?php
+		}
+	?>
+</ul>
+<?php 
+	unset($_SESSION['users']);
+	unset($_SESSION['users_names']);
+?>
