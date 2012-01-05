5c8a566376d1539e31df8058cb494de16354249c
silvan.staeheli@gmx.ch <silvan.staeheli@gmx.ch@81f5e6eb-a649-0410-baa0-7d6264f9b901>
2012-01-05 18:53:17 +0000

    automatisches mail bei registrierung

--- trunk/EM2008/Register/Register.php
+++ trunk/EM2008/Register/Register.php
@@ -72,6 +72,25 @@ class Register extends HTMLPage implements Page {
 			mysql_query($query,$this->link);
 
 		 if (mysql_affected_rows($this->link) > 0) {
+		 	if(!Constants::isLocal()){
+		 		$sender = "silvan.staeheli@gmx.ch";
+				$empfaenger = $this->email;
+				$betreff = "Erfolgreiche Registrierung bei tippy.ch";
+				$mailtext = "Hallo $this->vorname<br><br>
+				Super, hast du dich bei tippy.ch angemeldet. Ab sofort kannst du deine Tipps erfassen, im G&auml;stebuch Kommentare schreiben, 
+				die Statistiken betrachten oder Tipps der Konkurrenten ansehen. Ich rate dir jedoch (falls noch nicht geschehen), zuerst die 
+				Spielregeln zu lesen.<br><br>
+				Damit du auch tats&auml;chlich am Tippspiel teilnehmen kannst, ben&ouml;tige ich von dir noch Fr. 20.00 ! Falls du mich pers&ouml;nlich 
+				kennen solltest und mich auch in absehbarer Zeit sehen wirst, kannst du mir das Geld nat&uuml;rlich bar geben. Ansonsten rate ich dir, 
+				mir das Geld auf folgende Bankverbindung zu &uuml;berweisen:<br><br>
+				IBAN: CH75 0021 4214 4048 4640 T<br>
+				ADRESSE: Silvan St&auml;heli, Tannenrauchstrasse 102, 8038 Z&uuml;rich<br><br>
+				Dann bleibt mir nichts mehr anderes &uuml;brig als dir viel Gl&uuml;ck und Spass bei tippy zu w&uuml;nschen.<br><br>
+				Sportliche Gr&uuml;sse<br>
+				Silvan St&auml;heli";
+				mail($empfaenger, $betreff, $mailtext, "From: $sender\n" . "Content-Type: text/html; charset=iso-8859-1\n"); 
+		 	}
+		 	
 		 	$_SESSION['infos'][] = "Du wurdest erfolgreich registriert";
 
 		 	header('location:	index.php?go=login');
