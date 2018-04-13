577c8b417f40f26955212079a122942d6df5e351
Silvan <silvan.staeheli@bluewin.ch>
2018-04-13 19:57:37 +0200

    punkte & high risk tip

--- EM2008/Datenbank/db.php
+++ EM2008/Datenbank/db.php
@@ -2,7 +2,7 @@
 
 class Db {
 	static function getConnection() {
-		$link = @mysql_connect("localhost:8889", "root", "root") OR die(mysql_error());
+		$link = @mysql_connect("localhost", "root", "") OR die(mysql_error());
 		mysql_select_db("em2008") OR die(mysql_error());
 		
 		return $link;
